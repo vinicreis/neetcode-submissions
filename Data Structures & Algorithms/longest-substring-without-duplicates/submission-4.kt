1642768c15cf71cbf80fe2e1afb205bc5a0f02ae
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-07 20:47:12 -0300

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.kt
@@ -0,0 +1,81 @@
+class Solution {
+    /**
+
+        Given a string s, find the length of the longest substring without duplicate characters.
+
+        A substring is a contiguous sequence of characters within a string.
+
+     */
+    fun lengthOfLongestSubstring(s: String): Int {
+        return twoPointers(s)
+    }
+
+    /**
+         i  j      
+        "abcabcbb"
+
+         ij      
+        "asdfghjk"
+         ij      
+        "aaaaaaaa"
+         ij   
+        "pwwkew"
+
+        max = 3
+     */
+    private fun twoPointers(s: String): Int {
+        // Time: O(n)
+        // Space: O(1)
+
+        if (s.isEmpty()) return 0
+        if (s.length == 1) return 1
+
+        var i = 0
+        var j = 1
+        var maxSequence = 0
+        val found = hashMapOf<Char, Int>(s[i] to i)
+
+        while (j < s.length) {
+            if (s[j] in found) {
+                i = max(found[s[j]]!! + 1, i)
+            } 
+
+            found[s[j]] = j
+            maxSequence = max(maxSequence, j - i + 1)
+
+            j++
+        }
+
+        return maxSequence
+    }
+
+    /**
+        "hi! Itdoesnrpy"
+
+        - Edge cases:
+            - s is empty: 0
+            - case sensitive? yes
+            
+        - intuition (brute force)
+            - for every character, traverse the string looking for repeatition
+     */
+    private fun bruteForce(s: String): Int {
+        // Time: O(n²)
+        // Space: O(1)
+        if (s.isEmpty()) return 0
+
+        var maxSequence = 0
+
+        for (start in s.indices) {
+            for (end in start + 1 until s.length) {            
+                if (s[start] == s[end]) {
+                    maxSequence = max(maxSequence, end - start)
+
+                    break
+                }
+            }
+        }
+
+        return maxSequence
+    }
+}

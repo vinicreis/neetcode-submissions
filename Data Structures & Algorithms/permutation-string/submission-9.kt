05715ce19fbc8470c3ea16526b738a395e9bf6e3
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-08 12:07:05 -0300

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.kt
@@ -0,0 +1,89 @@
+class Solution {
+    /**
+
+        You are given two strings s1 and s2.
+
+        Return true if s2 contains a permutation of s1, or false otherwise. 
+        
+        That means if a permutation of s1 exists as a substring of s2, then return true.
+
+        Both strings only contain lowercase letters.
+
+     */
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        return solution2(s1, s2)
+    }
+
+    /**
+
+        - We saw that sorting helps
+        - A frequency map?
+            - 
+
+                        
+            lecabee -> abceeel  -> { a: 1, b: 1, c: 1, e: 3, l: 1 }
+            abc     -> abc      -> { a: 1, b: 1, c: 1 }
+
+        it does not ensure the order, which is not important
+
+     */
+    private fun solution2(s1: String, s2: String): Boolean {
+        if (s1.isEmpty()) return true
+        if (s2.isEmpty()) return false
+        if (s2.length < s1.length) return false
+
+        val s1Frequencies = hashMapOf<Char, Int>()
+        val s2Frequencies = hashMapOf<Char, Int>()
+
+        for (c in s1) {
+            s1Frequencies[c] = s1Frequencies.getOrDefault(c, 0) + 1
+        }
+
+        for (c in s2) {
+            s2Frequencies[c] = s2Frequencies.getOrDefault(c, 0) + 1
+        }
+
+        return s1Frequencies.all { (k, v) -> s1Frequencies.containsKey(k) && s2Frequencies[k] == v }
+    }
+
+    /**
+
+        s1 = "abc"
+        s2 = "lecabee"
+
+        brute force:
+            for i in s1
+                var found = ""
+
+                for j in s2
+                    if (s1[i] == s2[j])
+                        found += s2[j].toString()
+                    else 
+                        break
+
+                if (found == s2) return true
+
+            return false
+                    
+     */
+    private fun bruteForce(s1: String, s2: String): Boolean {
+        // Time: O(n * m)
+        // Space: O(n + m)
+        // where n = s1.length, m = s2.length
+        if (s1.isEmpty()) return true
+        if (s2.isEmpty()) return false
+        if (s2.length < s1.length) return false
+
+        val sortedS1 = s1.toCharArray().sorted().joinToString(separator = "")
+
+        for (i in s2.indices) {
+            for (j in i until s2.length) {
+                val substring = s2.substring(i, j + 1).toCharArray().sorted().joinToString(separator = "")
+
+                if (substring == sortedS1) return true
+            }
+        }
+
+        return false
+    }
+}

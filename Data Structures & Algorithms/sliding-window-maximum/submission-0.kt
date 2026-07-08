f7ca5571506ba770dc0fc98541855762161c318e
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-08 17:47:46 -0300

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.kt
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+
+        You are given an array of integers nums and an integer k.
+        
+        There is a sliding window of size k that starts at the left edge of the array.
+        
+        The window slides one position to the right until it reaches the right edge of the array.
+
+        Return a list that contains the maximum element in the window at each step.
+
+     */
+    fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+        return solution(nums, k)
+    }
+
+    private fun solution(nums: IntArray, k: Int): IntArray {
+        return intArrayOf()
+    }
+}

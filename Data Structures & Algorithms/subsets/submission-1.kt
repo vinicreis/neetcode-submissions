c9f59dee7f908bc1482229ce935d6cb6a69b5f2c
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-11 18:46:24 -0300

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        return dfs(nums)
+    }
+
+    private fun dfs(nums: IntArray): List<List<Int>> {
+        if (nums.isEmpty()) return emptyList()
+
+        val result = mutableListOf<List<Int>>()
+        val subset = mutableListOf<Int>()
+
+        fun dfs(i: Int = 0) {
+            if (i == nums.size) {
+                result.add(subset.toList()); return
+            }
+
+            subset.add(nums[i])
+            dfs(i + 1)
+            subset.removeAt(subset.size - 1)
+            dfs(i + 1)
+        }
+
+        dfs()
+
+        return result
+    }
+}

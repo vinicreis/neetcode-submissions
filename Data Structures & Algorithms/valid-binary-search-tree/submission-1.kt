469b5dbcfd6dbcb1312339d18b1182f1300671ba
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-10 22:43:44 -0300

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.kt
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun isValidBST(root: TreeNode?): Boolean {
+        return dfs(root)
+    }
+
+    /**
+
+        dfs
+        pass 
+
+
+     */
+    private fun dfs(root: TreeNode?): Boolean {
+        if (root == null) return true
+
+        fun dfs(node: TreeNode?, left: Int = Int.MIN_VALUE, right: Int = Int.MAX_VALUE): Boolean {
+            if (node == null) return true
+
+            if (node.`val` <= left || node.`val` >= right) return false
+
+            return dfs(node.left, left, right = node.`val`)
+                && dfs(node.right, left = node.`val`, right)
+        }
+        
+        return dfs(root)
+    }
+}

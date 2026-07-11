786e562f205e6a929abe94ae53311ea6ae523eb1
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-10 21:54:44 -0300

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.kt
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun goodNodes(root: TreeNode?): Int {
+        return dfs(root)
+    }
+
+    /**
+
+        To know if there is a greater value, we can
+            - pass the whole path
+            - pass the max value (?)
+
+     */
+    private fun dfs(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        var result = 0
+
+        fun dfs(node: TreeNode?, maxVal: Int? = null) {
+            if (node == null) return
+
+            if (maxVal == null || node.`val` <= maxVal) result++
+
+            dfs(node.left, max(maxVal!!, node.`val`))
+            dfs(node.right, max(maxVal!!, node.`val`))
+        }
+
+        dfs(root)
+
+        return result
+    }
+}

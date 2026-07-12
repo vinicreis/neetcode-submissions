04d2f3e710289ead07251580ff6ec79514888f3c
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-12 12:57:43 -0300

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun rob(nums: IntArray): Int {
+        return solution(nums)
+    }
+
+    private fun solution(nums: IntArray): Int {
+        if (nums.size <= 0) return 0
+        if (nums.size == 1) return nums.first()
+
+        val cache = IntArray(nums.size)
+
+        cache[0] = nums[0]
+        cache[1] = max(nums[0], nums[1])
+
+        for (i in 2 until nums.size) {
+            cache[i] = max(cache[i - 1], cache[i - 2] + nums[i])
+        }
+
+        return cache.last()
+    }
+}

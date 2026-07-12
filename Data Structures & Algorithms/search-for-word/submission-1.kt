62d112ce02ec0b4785d33f9daa801541a5944269
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-12 12:05:44 -0300

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.kt
@@ -0,0 +1,38 @@
+class Solution {
+    fun exist(board: Array<CharArray>, word: String): Boolean {
+        return backtracking(board, word)
+    }
+
+    private fun backtracking(board: Array<CharArray>, word: String): Boolean {
+        if (word.isEmpty()) return true
+        if (board.isEmpty()) return false
+
+        fun backtracking(
+            i: Int = 0,
+            j: Int = 0,
+            n: Int = 0,
+            visited: Array<BooleanArray> = Array(board.size) { BooleanArray(board[it].size) },
+        ): Boolean {
+            if (n == word.length) return true
+            if (i < 0 || i > board.lastIndex) return false
+            if (j < 0 || j > board[i].lastIndex) return false
+            if (visited[i][j]) return false
+            if (board[i][j] != word[n]) return false
+
+            visited[i][j] = true
+
+            return backtracking(i, j - 1, n + 1, visited)
+                || backtracking(i - 1, j, n + 1, visited)
+                || backtracking(i, j + 1, n + 1, visited)
+                || backtracking(i + 1, j, n + 1, visited)
+        }
+
+        for (i in board.indices) {
+            for (j in board[i].indices) {
+                if (backtracking(i, j)) return true
+            }
+        }
+
+        return false
+    }
+}

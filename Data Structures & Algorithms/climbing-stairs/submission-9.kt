28be3db4c24bb8d8547d0e6044667cc993e1ecd7
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-24 20:52:55 -0300

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.kt
@@ -0,0 +1,47 @@
+class Solution {
+    fun climbStairs(n: Int): Int {
+        return dp(n)
+    }
+
+    /**
+
+        [ 1, 2, ..., n ]
+          0, 1, ..., n + 1
+
+     */
+    private fun dp(n: Int): Int {
+        if (n < 0) return 0
+
+        val memo = IntArray(n + 1) { 0 }
+
+        memo[1] = 1
+        memo[2] = 2
+
+        for (i in 3 .. n) {
+            memo[i] = memo[i - 1] + memo[i - 2]
+        }
+
+        return memo[n - 1]
+    }
+
+    /**
+
+        Edge cases:
+            - n <= 0, return 0
+            - n == 1, return 1
+            - n == 2, return 2
+
+        Recursion
+            n, n - 1, ..., 0
+
+            solution(n - 1) + solution(n - 2)
+
+     */
+    private fun recursion(n: Int): Int {
+        if (n <= 0) return 0
+        if (n == 1) return 1
+        if (n == 2) return 2
+
+        return recursion(n - 1) + recursion(n - 2)
+    }
+}

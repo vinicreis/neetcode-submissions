dcf2fa9785a1042fa9915fae06d86b70fa112b86
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-28 13:57:15 -0300

    Add: coin-change - submission-10

---
+++ Data Structures & Algorithms/coin-change/submission-10.kt
@@ -0,0 +1,99 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        return bfs(coins, amount)
+    }
+
+    private fun bfs(coins: IntArray, amount: Int): Int {
+        if (coins.isEmpty()) return -1
+        if (amount == 0) return 0
+
+        val queue = ArrayDeque<Int>()
+        val visited = hashSetOf<Int>(0)
+        var result = 0
+
+        queue.addFirst(0)
+
+        while (queue.isNotEmpty()) {
+            result++
+
+            repeat(queue.size) {
+                val current = queue.removeFirst()
+
+                for (coin in coins) {
+                    val next = current + coin
+
+                    if (next == amount) return result
+                    if (next > amount) continue
+                    if (next in visited) continue
+
+                    visited.add(next)
+                    queue.addLast(next)
+                }
+            }
+        }
+
+        return -1
+    }
+
+    private fun dpTopDown(coins: IntArray, amount: Int): Int {
+        if (coins.isEmpty()) return -1
+
+        val memo = hashMapOf(0 to 0)
+
+        fun rec(remaining: Int = amount): Int {
+            if (remaining in memo) return memo[remaining]!!
+            if (remaining == 0) return 0
+
+            var result = INF
+            
+            for (coin in coins) {
+                if (remaining - coin >= 0) {
+                    result = min(result, 1 + rec(remaining - coin))
+                }
+            }
+
+            memo[remaining] = result
+
+            return result
+        }
+
+        return rec().takeIf { it < INF } ?: -1
+    }
+
+    /**
+
+        Edge cases:
+            - coins is empty, return -1
+            - amount <= 0, return -1
+
+        if coins is sorted, maybe we can choose where to go?
+        if coins are sorted in desc order, helps the solution?
+
+        amount = 12
+        coins  = [ 1, 5, 10 ]
+
+     */
+    private fun recursion(coins: IntArray, amount: Int): Int {
+        if (coins.isEmpty()) return -1
+
+        fun rec(remaining: Int = amount): Int {
+            if (remaining <= 0) return 0
+            
+            var result = INF
+            
+            for (coin in coins) {
+                if (remaining - coin >= 0) {
+                    result = min(result, 1 + rec(remaining - coin))
+                }
+            }
+
+            return result
+        }
+
+        return rec().takeIf { it < INF } ?: -1
+    }
+
+    companion object {
+        const val INF = 1000000000
+    }
+}

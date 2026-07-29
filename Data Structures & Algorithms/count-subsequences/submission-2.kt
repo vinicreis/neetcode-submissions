191ba40d318408073ff744fb6ed15a2d5fb4058b
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-29 14:03:10 -0300

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.kt
@@ -0,0 +1,63 @@
+class Solution {
+    /**
+
+        - Edge cases
+            - t is empty, return 1
+            - s is empty, return 0
+
+     */
+    fun numDistinct(s: String, t: String): Int {
+        if (t.isEmpty()) return 0
+        if (s.isEmpty()) return 0
+
+        return dp(s, t)
+    }
+
+    private fun dp(s: String, t: String): Int {
+        val memo = hashMapOf<Pair<Int, String>, Int>()
+
+        fun dfs(i: Int = 0, cur: String = ""): Int {
+            if (cur == t) return 1
+            if (cur.length > t.length) return 0
+            if (i > s.lastIndex) return 0
+            if ((i to cur) in memo) return memo[i to cur]!!
+
+            val result = dfs(i + 1, cur) + dfs(i + 1, cur + s[i])
+
+            memo[i to cur] = result
+
+            return result
+        }
+
+        return dfs()
+    }
+
+    /**
+
+        To find all subsequences, we can make a kind of backtracking
+        We use the choices, for each character on s:
+            1. Add char
+            2. Skip char
+
+        for a current text cur:
+            if (cur == t) return 1
+            if (cur.length != t.length) return 0
+            if (i > s.length) return 0
+
+
+        Use a recursion to find subsequence?
+
+     */
+    private fun intuition(s: String, t: String): Int {
+        fun dfs(i: Int = 0, cur: String = ""): Int {
+            // println("Current: $cur")
+            if (cur == t) return 1
+            if (cur.length > t.length) return 0
+            if (i > s.lastIndex) return 0
+
+            return dfs(i + 1, cur) + dfs(i + 1, cur + s[i])
+        }
+
+        return dfs()
+    }
+}

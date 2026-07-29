6d9c87971f55ac136d65ed7eedba2399543db212
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-29 00:02:56 -0300

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.kt
@@ -0,0 +1,66 @@
+class Solution {
+    /**
+
+        Edge cases:
+            if any text is empty, return 0
+
+     */
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        if (text1.isEmpty()) return 0
+        if (text2.isEmpty()) return 0
+
+        return dfs(text1, text2)
+    }
+
+    private fun dfs_dp_bottomUp(text1: String, text2: String): Int {
+        val memo = Array(text1.size) { IntArray(text2.size) { -1 } }
+
+        fun dfs(i: Int = 0, j: Int = 0): Int {
+            if (i >= text1.length) return 0
+            if (j >= text2.length) return 0
+
+            return if (text1[i] != text2[j]) {
+                max(dfs(i + 1, j), dfs(i, j + 1))
+            } else {
+                1 + dfs(i + 1, j + 1)
+            }
+        }
+
+        return dfs()
+    }
+
+    /**
+                   i
+                        i
+                        i    i
+        text1 = [ 'c', 'a', 't' ]
+                        j    j 
+        text2 = [ 'c', 'r', 'a', 'b', 't' ]
+
+        fun dfs(i: Int = 0, j: Int = 0): Int {
+            if (i >= text1.length) return 0
+            if (j >= text2.length) return 0
+
+            return if (text1[i] != text2[j]) {
+                dfs(i + 1, j) + dfs(i, j + 1)
+            } else {
+                1 + dfs(i + 1, j + 1)
+            }
+        }
+
+     */
+    private fun dfs(text1: String, text2: String): Int {
+        fun dfs(i: Int = 0, j: Int = 0): Int {
+            if (i >= text1.length) return 0
+            if (j >= text2.length) return 0
+
+            return if (text1[i] != text2[j]) {
+                max(dfs(i + 1, j), dfs(i, j + 1))
+            } else {
+                1 + dfs(i + 1, j + 1)
+            }
+        }
+
+        return dfs()
+    }
+}

100475ae9a706b4542428c95ff194d5add327a4e
Vinícius Reis <vini.reis@outlook.com.br>
2026-07-29 09:28:40 -0300

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.kt
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+
+        Edge cases
+            - if nums is empty, return 0
+
+     */
+    fun findTargetSumWays(nums: IntArray, target: Int): Int {
+        if (nums.isEmpty()) return 0
+
+        return dp(nums, target)
+    }
+
+    private fun dp(nums: IntArray, target: Int): Int {
+        // Time:  O(2^n)
+        // Space: O(n)
+        val memo = hashMapOf<Pair<Int, Int>, Int>()
+
+        fun dfs(i: Int = 0, remaining: Int = target): Int {
+            if (i == nums.size && remaining == 0) return 1
+            if (i > nums.lastIndex) return 0
+            if ((i to remaining) in memo) return memo[i to remaining]!!
+
+            memo[i to remaining] = dfs(i + 1, remaining + nums[i]) + dfs(i + 1, remaining - nums[i])
+
+            return memo[i to remaining]!!
+        }
+
+        return dfs()
+    }
+
+    /**
+
+        - for each coin
+            - sum coin
+            - minus coin
+
+        when reach target, increment
+        recursion? dfs?
+
+     */
+    private fun intuition(nums: IntArray, target: Int): Int {
+        // Time:  O(2^n)
+        // Space: O(n)
+        fun dfs(i: Int = 0, remaining: Int = target): Int {
+            if (i == nums.size && remaining == 0) return 1
+            if (i > nums.lastIndex) return 0
+
+            return dfs(i + 1, remaining + nums[i]) + dfs(i + 1, remaining - nums[i])
+        }
+
+        return dfs()
+    }
+}
